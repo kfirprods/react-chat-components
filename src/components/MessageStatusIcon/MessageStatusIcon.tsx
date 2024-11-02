990972bc22ecc86af4e878667e3ee30e1e4ffee8
Kfir Eichenblat <kfirprods@gmail.com>
2024-11-02 16:52:06 +0100

    Fix: added missing icon for the "FAILED" status of a message

--- src/components/MessageStatusIcon/MessageStatusIcon.tsx
+++ src/components/MessageStatusIcon/MessageStatusIcon.tsx
@@ -30,6 +30,12 @@ export default function MessageStatusIcon({
     </svg>
   );
 
+  const errorIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
+    </svg>
+  );
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -39,9 +45,12 @@ export default function MessageStatusIcon({
       stroke="currentColor"
       className={clsx("size-4 inline", {
         "text-blue-500": status === ChatMessageStatus.SEEN,
+        "text-red-400": status === ChatMessageStatus.FAILED,
+        "size-6": status === ChatMessageStatus.FAILED,
       })}
     >
       {status === ChatMessageStatus.SENDING && clockIcon}
+      {status === ChatMessageStatus.FAILED && errorIcon}
 
       {shouldRenderTick && (
         <path
