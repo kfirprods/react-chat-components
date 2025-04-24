efeeb6821d4e75b97744d8186c9b4c9050e4dbf5
Kfir <kfir@alignedup.com>
2025-04-24 10:16:41 -0700

    and now

--- src/stories/Chat.stories.tsx
+++ src/stories/Chat.stories.tsx
@@ -5,7 +5,7 @@ import { useCallback, useState } from "react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Higher Level Components/Chat",
+  title: "Higher Level Components/Chat SEE THIS",
   component: Chat,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
